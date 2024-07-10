15d627ed8638870b025a34f63e1e9091a4618a0f
sergi.hurtado <sergi.hurtado@cotecna.es>
2024-07-10 09:10:24 +0200

    Add deeplinks

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,63 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_geo_poc/screens/home_page_screen.dart';
 import 'package:go_router/go_router.dart';
 
+
+
 void main() {
-  runApp(MaterialApp.router(
-    routerConfig: router, 
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _linkSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initDeepLinks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     title: 'Flutter Demo', 
     theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),));
-}
+      ),
+    home: const HomePageScreen(),
+    );
+  }
 
-final router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (_, __) => const HomePageScreen(),
-      routes: const [],
-    ),
-  ],
-);
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+
+    super.dispose();
+  }
+
+  Future<void> initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // Handle links
+    _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
+      debugPrint('onAppLink: $uri');
+      openAppLink(uri);
+    });
+  }
+
+  void openAppLink(Uri uri) {
+    print("OPEN DEEPLINK");
+  }
+}
